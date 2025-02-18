803e49c6c799e18d961b467aaedefe1d1bc5591a
Anitarwrwrw <anitarahmawati676@gmail.com>
2025-02-18 17:26:25 +0700

    buat file variable

---
+++ Variable.php
@@ -0,0 +1,27 @@
+YANG UDAH SAYA RUNNING HASILNYA INI:
+
+PS D:\belajar-php> php variable.php
+anettanettanettanettanettanettanettanett
+PS D:\belajar-php> php variable.php
+napiknapiknapiknapiknapiknapiknapiknapik
+PS D:\belajar-php> php variable.php
+Name :napik
+Umur :23
+PS D:\belajar-php> php variable.php
+Name :Anita Rahmawati
+Umur :23
+PS D:\belajar-php>
+
+<?php
+
+$name = "Anita Rahmawati";
+$age = 23;
+
+echo "Name :";
+echo $name;
+echo "\n";
+
+echo "Umur :";
+echo $age;
+
+
